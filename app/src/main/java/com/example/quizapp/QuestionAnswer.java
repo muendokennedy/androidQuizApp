03008bf0a9291da3401221b5fe7e57f7527581de
muendokennedy <kennedymunyao999@gmail.com>
2023-11-27 23:14:59 +0300

    Created the class that contains questions and answers as well as choices

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public  static String question[] = {
+            "Which company owns the Android?",
+            "Which one is not a programming language?",
+            "What is the most popular online video platform in the world?"
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Facebook", "Whatsapp", "Instagram", "YouTube"}
+    };
+
+    public static String correctAnswers[] = {
+           "Google",
+           "Notepad",
+           "YouTube"
+    };
+}
